9032d9425c803ea89a4edd09ab9830847ead3c68
Alandis Seals <alandisseals@Alandiss-MacBook-Air.local>
2020-10-18 01:17:48 -0500

    Sign In Methods

---
+++ Social Squid/Social Squid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Squid
+//
+//  Created by Alandis Seals on 10/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
